772586ff978f1b3ac55f856ca92ee180faa8e97a
Aaron Kaka <aaron.kaka@pearson.com>
2016-04-22 14:09:04 -0600

    fix: Use verify npm script to ensure package name has @pearson-components scope.

--- npm_scripts/verify.js
+++ npm_scripts/verify.js
@@ -1,7 +1,13 @@
 const log = require('./log');
 const path = require('path');
 const fs = require('fs');
+const pkg = require('../package.json');
 const bowerPath = path.join(process.cwd(), '/bower.json');
+
+if (pkg.name.indexOf('@pearson-components/') === -1) {
+  log.primaryError('Package name must be pre-pended with "@pearson-components" scope.');
+  process.exit(1);
+}
 
 if (!fs.existsSync(bowerPath)) {
   log.secondary('Validated project.');
