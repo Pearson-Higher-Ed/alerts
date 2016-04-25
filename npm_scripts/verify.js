3d2516cbd1279540e289d46998549a76a92f8ea3
Aaron Kaka <aaron.kaka@pearson.com>
2016-04-25 12:49:18 -0600

    refactor: Make verify npm script DRY.

--- npm_scripts/verify.js
+++ npm_scripts/verify.js
@@ -3,30 +3,29 @@ const path = require('path');
 const fs = require('fs');
 const pkg = require('../package.json');
 const bowerPath = path.join(process.cwd(), '/bower.json');
+const exitFailure = (message) => {
+  log.primaryError(message);
+  process.exit(1);
+};
 
 if (!pkg.name.startsWith('@pearson-components/')) {
-  log.primaryError('Package name must be pre-pended with "@pearson-components/" scope.');
-  process.exit(1);
+  exitFailure('Package name must be pre-pended with "@pearson-components/" scope.');
 }
 
 if (!pkg.description) {
-  log.primaryError('Package description must be provided.');
-  process.exit(1);
+  exitFailure('Package description must be provided.');
 }
 
 if (!pkg.author) {
-  log.primaryError('Package author must be provided.');
-  process.exit(1);
+  exitFailure('Package author must be provided.');
 }
 
 if (!pkg.repository || !pkg.repository.url || pkg.repository.url === 'https://github.com/Pearson-Higher-Ed/') {
-  log.primaryError('Package repository url must be provided with repo name.');
-  process.exit(1);
+  exitFailure('Package repository url must be provided with repo name.');
 }
 
 if (fs.existsSync(bowerPath)) {
-  log.primaryError('No Bower configuration or dependencies allowed.');
-  process.exit(1);
+  exitFailure('No Bower configuration or dependencies allowed.');
 }
 
 log.primary('Verified project.');
