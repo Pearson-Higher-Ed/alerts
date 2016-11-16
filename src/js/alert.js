5b7703576f5eb3f9ae0f8f17f7affc9c55af17e3
Mike <Mhomolak@yahoo.com>
2016-11-16 13:40:39 -0700

    chore: aria

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Alert = (props) => (
-    <span className={`alert-span ${props.closeTitleProp}`} style={{opacity: props.opacity}}>
+    <span className={`alert-span ${props.closeTitleProp}`} role="alert" style={{opacity: props.opacity}}>
       <span className="alert-title">
       <strong>Error</strong>
-      <span className="close-title" onClick={props.handleClose}>&times;</span>
+      <button className="close-title" onClick={props.handleClose} aria-label="Close alert" >&times;</button>
       </span><br/>
       <span className="alert-body">Uh oh, we weren&apos;t able to verify your email address.
       Try resending the email.
