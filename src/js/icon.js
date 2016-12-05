e3511a08462c51e9fbc0d3b8a8952b1e608e6730
Mike <Mhomolak@yahoo.com>
2016-12-05 15:26:01 -0700

    fix: Icons rendering to the alert

---
+++ src/js/icon.js
@@ -0,0 +1,16 @@
+const Icon = (props) => {
+
+  const icon_class = 'pe-icon--' + props.name;
+
+  return(
+    <svg version = "1.1"
+         xmlns = "http://www.w3.org/2000/svg"
+         xmlnsXlink = "http://www.w3.org/1999/xlink"
+         aria-hidden = "true"
+         className = {icon_class}>
+      <use xlinkHref = {'#' + props.name}></use>
+    </svg>
+  );
+}
+
+export default Icon;
