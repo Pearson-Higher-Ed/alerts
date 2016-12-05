bd3a43f33439b752cc3aa9a7ac5a8eb4be5e4f73
mallory <stommepoes@stommepoes.nl>
2016-12-05 23:12:45 +0100

    test

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
-import Icon from './icon';
+const Icon = (props) => {
+
+  const icon_class = 'pe-icon--' + props.name;
+
+  return(
+    <svg version = "1.1"
+         xmlns = "http://www.w3.org/2000/svg"
+         xmlnsXlink = "http://www.w3.org/1999/xlink"
+         aria-hidden = "true"
+         className = {icon_class}>
+      <use xlinkHref = {'#' + props.name}></use>
+    </svg>
+  );
+}
 
 const Alert = (props) => (
   <span className={`alert-span ${props.closeTitleProp}`} id={`${props.whichAlertProp}`} role="alert" style={{opacity: props.opacity}}>
