c923f637cd6cea748e39bc68911d0893fb3ccba2
Mike <Mhomolak@yahoo.com>
2017-01-19 09:43:29 -0700

    INTL rendering both types just not quite properly yet

--- src/js/AlertList.js
+++ src/js/AlertList.js
@@ -1,8 +1,16 @@
-import React, { Component } from 'react';
-import Alert                from './Alert';
-
+import React, { Component, PropTypes } from 'react';
+import Alert from './Alert';
+import { intlShape, injectIntl } from 'react-intl';
+import { messages } from '../../translations/defaultMessages';
 
 class AlertList extends Component {
+
+  static propTypes = {
+    intl: intlShape.isRequired,
+    data: PropTypes.shape({
+      locale: PropTypes.string
+    })
+  };
 
   constructor(props) {
     super(props);
@@ -25,7 +33,7 @@ class AlertList extends Component {
 }
 
 
-export default AlertList;
+export default injectIntl(AlertList);
 
 
 function _handleClose (currentIndex) {
@@ -33,16 +41,31 @@ function _handleClose (currentIndex) {
 }
 
 function _renderAlert (alertList, closeIndex) {
-  const alertListFiltered = alertList.filter((e, index) => index !== closeIndex)
-  this.state.closeIndex   = undefined;
+  const alertListFiltered = alertList.filter((e, index) => index !== closeIndex);
   this.state.alertList    = alertListFiltered;
+  const { intl } = this.props;
+
   return alertListFiltered.map((alert, index) =>
+
+    (this.state.alertList[(this.state.alertList.length) - 1].alertType) === 'success' ?
+
     <Alert
       index        = {index}
       closeIndex   = {closeIndex}
       key          = {index}
-      alertType    = {alert.alertType}
-      alertMessage = {alert.alertMessage}
+      alertType    = {intl.formatMessage(messages.successAlert)}
+      alertMessage = {intl.formatMessage(messages.successAlert)}
+      handleClose  = {this.handleClose}
+    />
+
+    :
+
+    <Alert
+      index        = {index}
+      closeIndex   = {closeIndex}
+      key          = {index}
+      alertType    = {intl.formatMessage(messages.errorAlert)}
+      alertMessage = {intl.formatMessage(messages.errorAlert)}
       handleClose  = {this.handleClose}
     />
   );
