edd517982e6f2404b103c33b66450b54c396490c
Mike <Mhomolak@yahoo.com>
2017-01-30 15:12:41 -0700

    Width change at responsive BP and capitalized files actually going to GitHub.

--- src/js/Icon.js
+++ src/js/Icon.js
@@ -7,6 +7,7 @@ const Icon = (props) => {
        xmlns = "http://www.w3.org/2000/svg"
        xmlnsXlink = "http://www.w3.org/1999/xlink"
        aria-hidden = "true"
+       focusable = "no"
        className = {icon_class}>
       <use xlinkHref = {'#' + props.name}></use>
     </svg>
