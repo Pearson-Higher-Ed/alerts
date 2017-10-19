cc34d908bcc39190288d767701308a1ea4177a39
Mike <Mhomolak@yahoo.com>
2017-10-19 16:17:27 -0600

    refactor: Remove react-intl dependency, change Alert to functional component

--- src/js/Alert.js
+++ src/js/Alert.js
@@ -1,52 +1,45 @@
-import React, { Component } from 'react';
-import Icon                 from './Icon';
+import React from 'react';
+import Icon  from './Icon';
 
-export default class Alert extends Component {
-
-  alertTypeCheck = () => {
-    if (this.props.alertType === 'Error') {
+const Alert = ({ index, alertType, handleClose, alertTitle, alertMessage }) => {
+  const alertTypeCheck = () => {
+    if (alertType === 'Error') {
       return (
         <span className="error-svg">
           <Icon name="warning-18" />
         </span>
       );
     }
-    if (this.props.alertType === 'Success') {
+    if (alertType === 'Success') {
       return (
         <span className="success-svg">
           <Icon name="check-sm-18" />
         </span>
       )
     }
-  }
+  };
 
-  render () {
-    const { index, alertType, handleClose, alertTitle, alertMessage } = this.props;
-    const infoCheck = alertType === 'Information' ? '-info' :'';
+  const infoCheck = alertType === 'Information' ? '-info' :'';
+  
+  return (
+    <li className ="pe-alert" id = {`alert-${alertType}-${index}`}>
+      <button className="pe-icon--btn close-title"
+              onClick={() => handleClose(index)}
+              aria-label="Close alert">
+        <Icon name="remove-sm-24" />
+      </button> <br/>
 
-    return (
-      <li className ="pe-alert"
-          id = {`alert-${alertType}-${index}`}>
-
-        <button className="pe-icon--btn close-title"
-                onClick={() => handleClose(index)}
-                aria-label="Close alert">
-
-          <Icon name="remove-sm-24" />
-
-        </button> <br/>
-
-        <div className="alert-content-container">
-          {this.alertTypeCheck()}
-          <div className={`alert-content${infoCheck}`}>
-            <h2 className="pe-label alert-title">
-              <strong>{alertTitle}</strong>
-            </h2>
-            <p className="pe-paragraph alert-text">{alertMessage}</p>
-          </div>
+      <div className="alert-content-container">
+        {alertTypeCheck()}
+        <div className={`alert-content${infoCheck}`}>
+          <h2 className="pe-label alert-title">
+            <strong>{alertTitle}</strong>
+          </h2>
+          <p className="pe-paragraph alert-text">{alertMessage}</p>
         </div>
-      </li>
-    );
-  }
-
+      </div>
+    </li>
+  );
 };
+
+export default Alert;
