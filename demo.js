0db839a396e7a078dcf2eca910e8e70e497ab5f9
Mike <Mhomolak@yahoo.com>
2017-11-30 13:32:14 -0700

    test

---
+++ demo.js
@@ -0,0 +1,10 @@
+function init() {
+  document.body.dispatchEvent(new CustomEvent('o.InitAlertsComponent', {
+    detail: {
+      elementId: 'demo-target1'
+    }
+
+  }) );
+}
+
+window.onload = init;
